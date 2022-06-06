e6d847858cfe30e720c0c214ca14c7be1d3953d2
Feconyh <carlosedu445c@gmail.com>
2022-06-06 16:54:15 -0300

    Updade função frame

--- TK/2.2_Botao soma.py
+++ TK/2.2_Botao soma.py
@@ -4,6 +4,9 @@ janela = Tk()
 janela.geometry('600x600+600+100')
 janela.config(background='lightblue')
 
+f1 = Frame(janela)
+f1.grid()
+
 def aumenta():
     if int(numero["text"]) < 10:
         numero["text"] = int(numero["text"]) + 1
@@ -17,10 +20,10 @@ janela.grid_columnconfigure(0, weight=1)
 janela.grid_columnconfigure(1, weight=1)
 janela.grid_columnconfigure(2, weight=1)
 
-numero = Label(janela, text='0', background='lightblue', font='Arial 15')
+numero = Label(f1, text='0', background='lightblue', font='Arial 15')
 
-btn1 = Button(janela, text='Botão 1', font='Arial 15', command=diminui)
-btn2 = Button(janela, text='Botão 2', font='Arial 15', command=aumenta)
+btn1 = Button(f1, text='Botão 1', font='Arial 15', command=diminui)
+btn2 = Button(f1, text='Botão 2', font='Arial 15', command=aumenta)
 
 numero.grid(row=0, column=1, stick=NSEW)
 
